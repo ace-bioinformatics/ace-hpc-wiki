549d0522a08425a5cb2679b5fa2a6b61ea86a02e
IannnnnW <ianwamai@gmail.com>
2025-06-30 17:02:45 +0300

    chore: Update the baseUrl for the wiki

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://ace-bioinformatics.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ace-hpc-wiki/',
+  baseUrl: '/ace-ug-hpc-wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
